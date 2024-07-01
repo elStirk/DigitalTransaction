801a46a4cfd1ad355e7f5e4a1531d3341f654cf5
Stirk <pablo.stirk.03@icloud.com>
2024-06-30 22:03:18 -0600

    a

---
+++ src/main/java/net/rpgmyth/digitaltransactions/DigitalTransactions.java
@@ -0,0 +1,25 @@
+package net.rpgmyth.digitaltransactions;
+
+import net.rpgmyth.digitaltransactions.events.TransactionTax;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DigitalTransactions extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        registerEvents();
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    public void registerEvents(){
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(new TransactionTax(), this);
+    }
+
+
+}
---
+++ src/main/java/net/rpgmyth/digitaltransactions/events/TransactionTax.java
@@ -0,0 +1,11 @@
+package net.rpgmyth.digitaltransactions.events;
+
+import net.essentialsx.api.v2.events.TransactionEvent;
+import org.bukkit.event.Listener;
+
+
+public class TransactionTax implements Listener {
+    public void onTransaction(TransactionEvent event){
+
+    }
+}
